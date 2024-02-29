342deb4449a104a4376b536d3c44411fca5eefa1
Alexey_Yamushev/dae56 <Privet.02@mail.ru>
2024-02-29 18:17:39 +0400

    refactor alembic to async

--- src/user/models.py
+++ src/user/models.py
@@ -1,5 +1,6 @@
 from database import Base, intpk
 
+from sqlalchemy import String
 from sqlalchemy.orm import mapped_column, Mapped
 from typing_extensions import TYPE_CHECKING
 
@@ -10,4 +11,10 @@ if TYPE_CHECKING:
 class User(Base):
     __tablename__ = "users"
 
+    id: Mapped[intpk]
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    middle_name: Mapped[str] = mapped_column(String(50), nullable=False)
 
+
+metadata = User.metadata
